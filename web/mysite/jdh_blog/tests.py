bbd89ddb5d85e06f531ef34689f7c3051966a6bf
dahui.jiang <jiangdahui000@gmail.com>
2017-10-26 07:42:48 +0000

    Add unit tests.

--- web/mysite/jdh_blog/tests.py
+++ web/mysite/jdh_blog/tests.py
@@ -1,3 +1,14 @@
+from datetime import datetime
 from django.test import TestCase
+#from django.test.client import Client
+from jdh_blog.models import BlogPost
 
-# Create your tests here.
+class BlogPostTest(TestCase):
+    def test_obj_create(self):
+        BlogPost.objects.create(title = 'raw_file', 
+                body = 'raw_body', timestamp = datetime.now())
+        self.assertEqual(1, BlogPost.objects.count())
+        self.assertEqual('raw_file', BlogPost.objects.get(id = 1).title)
+    def test_slash(self):
+        response = self.client.get('/')
+        self.assertIn(response.status_code, (301, 302))
