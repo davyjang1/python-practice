a53400f7acabed3b22e324f160bc73d3e6068e76
dahui.jiang <jiangdahui000@gmail.com>
2018-03-07 18:41:31 -0800

    fix bug

--- thread/paxos_basic_demo.py
+++ thread/paxos_basic_demo.py
@@ -2,6 +2,7 @@ from threading import Thread
 from queue import Queue
 from math import floor
 import time
+import copy
 
 
 # cause some trouble here, either absent or not vote
@@ -10,131 +11,169 @@ def start_priest(Id, msg_queues, priest_ids, decs):
 	ballots = {}
 	passed_decs = []
 
-	lastTried = 0
+	lastTried = Id
 	prevVotes = {} # like prevVotes[id] = [] or lastest dec
+	re_prevVotes = []
 
 	# need reinit begin
-	state = "idle"
+	status = "idle"
 	quorum = []
 	voters = []
 	decree = None
 	outcome = None
+	owner = {}
 	# need reinit end
 
 
 	nextBal = None
 	prevBal = None
 	prevDec = None
-	
-	if Id == 0:
-		state = "trying"
+
+	# assume that priest_ids[0,1] are the leaders
+	if Id == priest_ids[0] or Id == priest_ids[1]:
+	#if Id == priest_ids[1]:
+		status = "trying"
+
+	# to mock prevBal and prevDec
+	if Id == priest_ids[3]:
+		#time.sleep(3)
+		prevBal = 0
+		prevDec = 2
+
+
+	if "trying" == status:
+		lastTried += 1
+		for other in priest_ids:
+			msg = {}
+			msg['type'] = "NextBallot"
+			msg['ballot_num'] = lastTried
+			print ("p " + str(Id) + " send " + str(msg) + " to " + str(other))
+			msg_queues[Id][other].put(msg)	
 
 	while(1):
-
 		for other in priest_ids:
-			item = msg_queues[other][Id].get()
+			#print (str(Id) + " try to get from " + str(other) + " queue's size:" + str(msg_queues[other][Id].qsize()))
+			if msg_queues[other][Id].empty():
+				item = None
+			else:
+				item = msg_queues[other][Id].get(block=False)
 			if item:
-			    msg_type = item['type']
+				msg_type = item['type']
 				if "NextBallot" == msg_type:
+					print ("p " + str(Id) + " recieve " + str(item) + " from " + str(other))
 					if None == nextBal or item['ballot_num'] >= nextBal:
 						nextBal = item['ballot_num']
-						#TODO record the owner of nextBal
+						owner[nextBal] = other
+					if None == prevBal or nextBal > prevBal:
+						msg = {}
+						msg['type'] = "LastVote"
+						msg['nextBal'] = nextBal
+						msg['prevBal'] = prevBal
+						msg['prevDec'] = prevDec
+						print ("p " + str(Id) + " send " + str(msg) + " to " + str(other))
+						msg_queues[Id][other].put(msg)
 
 				elif "LastVote" == msg_type:
-					#print ("Recieve LastVote from " + str(other) + " " + str(item))
-					if item['NextBal'] == lastTried and "trying" == status:
-						if (item['prevBal'] and item['prevDec']):
+					print ("p " + str(Id) + " recieve " + str(item) + " from " + str(other))
+					re_prevVotes.append(other)
+					if item['nextBal'] == lastTried and "trying" == status:
+						if (None != item['prevBal'] and None != item['prevDec']):
 							# later compare, just compare between the last ones 
 							prevVotes.setdefault(other, []).append({"bal": item['prevBal'],
 												"dec": item["prevDec"]})
+
+					cnt = floor(len(priest_ids) / 2) + 1
+					print ("cnt:" + str(cnt))
+					if len(re_prevVotes) >= cnt and "trying" == status:
+						for present in re_prevVotes:
+							if 0 == cnt:
+								break
+							quorum.append(present)
+							cnt -= 1
+						status = "polling"
+						voters = []
+						maxBal = -1
+
+						for key in prevVotes.keys():
+							if prevVotes[key][-1]["bal"] > maxBal:
+								maxBal = prevVotes[key][-1]["bal"]
+								decree = prevVotes[key][-1]["dec"]
+						if not (decree and decree not in passed_decs):
+							decree = decs[0]
+						
+						ballots[lastTried] = {"dec" : decree, "qrm": quorum, 'vate':[]}
+
+
+						for quo in quorum:
+							msg = {}
+							msg['type'] = "BeginBallot"
+							msg["ballot_num"] = lastTried
+							msg["decree"] = decree
+							print ("p " + str(Id) + " send " + str(msg) + " to " + str(quo))
+							msg_queues[Id][quo].put(msg)
+
 				elif "BeginBallot" == msg_type:
-					if item["ballot_num"] == nextBal and (not prevBal or nextBal > prevBal):
+					print ("p " + str(Id) + " recieve " + str(item) + " from " + str(other))
+					if item["ballot_num"] == nextBal and (None == prevBal or nextBal > prevBal):
 						prevBal = nextBal
 						prevDec = item['decree']
+						#TODO store in stable strorage
+						msg = {}
+						msg['type'] = "Voted"
+						msg['prevBal'] = prevBal
+						print ("p " + str(Id) + " send " + str(msg) + " to " + str(other))
+						msg_queues[Id][other].put(msg)
 
 				elif "Voted" == msg_type:
+					print ("p " + str(Id) + " recieve " + str(item) + " from " + str(other))
 					if item['prevBal'] == lastTried and "polling" == status:
 						voters.append(other)
+					if "polling" == status and set(quorum).issubset(set(voters)) and None == outcome:
+						outcome = decree
+					if None != outcome:
+						for in_other in priest_ids:
+							msg = {}
+							msg['type'] = "Success"
+							msg['outcome'] = outcome
+							print ("p " + str(Id) + " send " + str(msg) + " to " + str(in_other))
+							msg_queues[Id][in_other].put(msg)
+						status = "trying"
+						quorum = []
+						voters = []
+						decree = None
+						outcome = None
+						owner = {}
+						re_prevVotes = []
+						lastTried += 1
+						for other in priest_ids:
+							msg = {}
+							msg['type'] = "NextBallot"
+							msg['ballot_num'] = lastTried
+							print("p " + str(Id) + " send " + str(msg) + " to " + str(other))
+							msg_queues[Id][other].put(msg)
+
+
 
 				elif "Success" == msg_type:
-					if not outcome:
-						outcome = item['outcome']		
+					print ("p " + str(Id) + " recieve " + str(item) + " from " + str(other))
+					if None == outcome:
+						outcome = item['outcome']
+						print("p " + str(Id) + ":The outcome is " + str(outcome))
 					if outcome not in passed_decs:
 						passed_decs.append(outcome)
-					if set(decs).issubset(set(passed_decs)):
+						decs.remove(outcome)
+						
+					if not len(decs):
 						return
 
-			if status == "trying":
-				msg = {}
-				msg['type'] = "NextBallot"
-				msg['ballot_num'] = lastTried + 1
-				msg_queues[Id][other].put(msg)
-
-				if len(prevVotes.keys()) > 0:
-					cnt = floor(len(prevVotes.keys()) / 2) + 1
-					for present in prevVotes.keys():
-						if 0 == cnt:
-							break
-						quorum.append(present)
-						cnt -= 1
-					status = "polling"
-					voters = []
-					maxBal = -1
-
-					for key in prevVotes.keys():
-						if prevVotes[key][-1]["bal"] > maxBal:
-							maxBal = prevVotes[key][-1]["bal"]
-							decree = prevVotes[key][-1]["dec"]
-					if not decree:
-						decree = decs[0]
-					
-					ballots[lastTried] = {"dec" : decree, "qrm": quorum, 'vate':[]}
-
-			if None == prevBal or nextBal > prevBal:
-				msg = {}
-				msg['type'] = "LastVote"
-				msg['nextBal'] = nextBal
-				msg['prevBal'] = prevBal
-				msg['prevDec'] = prevDec
-				msg_queues[Id][other].put(msg)
-
-			if "polling" == status:
-				msg = {}
-				msg['type'] = "BeginBallot"
-				msg["ballot_num"] = lastTried
-				msg["decree"] = decree
-				if other in quorum:
-					msg_queues[Id][other].put(msg)
-
-			if prevBal:
-				msg = {}
-				msg['type'] = "Voted"
-				msg['prevBal'] = prevBal
-				msg_queues[Id][other].put(msg)
-
-
-			if "polling" == status and set(quorum).issubset(set(voters)) and not outcome:
-				outcome = decree
-
-			if outcome:
-				msg = {}
-				msg['type'] = "Success"
-				msg['outcome'] = outcome
-				msg_queues[Id][other].put(msg)
-				state = "idle"
-				quorum = []
-				voters = []
-				decree = None
-				outcome = None
-
-		time.sleep(3)
+		time.sleep(1)
 
 def main():
 	threads = []
 	priest_ids = []
 	priest_num = 5
 	msg_queues = {}
-	decs = {0 : "No.0 dec", 1 : "No.1 dec", 2 : "No.2 dec", 3 : "No.3 dec"}
+	decs = {0 : "No.0 dec"}
 	#TODO owner
 
 	for i in range(priest_num):
@@ -147,7 +186,8 @@ def main():
 
 	for i in range(priest_num):
 		priest_ids.append(i)
-		t = Thread(target = start_priest, args = (i, msg_queues, priest_ids, decs))
+		dec_ids = list(decs.keys())
+		t = Thread(target = start_priest, args = (i, msg_queues, priest_ids, copy.copy(dec_ids)))
 		threads.append(t)
 	
 	for i in range(priest_num):
